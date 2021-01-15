3934d8e21dec8e22bc9a7f4d9adf9e9826056f47
Salih Erkan SANDAL <saliherkansandal@gmail.com>
2021-01-16 02:16:48 +0300

    Cell Oluşturuldu

--- EtsMobileApp/EtsMobileApp/ViewController.swift
+++ EtsMobileApp/EtsMobileApp/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 150
+        return 160
         
     }
    
