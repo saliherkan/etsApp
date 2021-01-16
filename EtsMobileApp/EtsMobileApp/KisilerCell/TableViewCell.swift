4ce675a6ffc1e64b85164babd4786eb4e4d68137
Salih Erkan SANDAL <saliherkansandal@gmail.com>
2021-01-16 22:13:19 +0300

    Dashed Line Eklendi

--- EtsMobileApp/EtsMobileApp/KisilerCell/TableViewCell.swift
+++ EtsMobileApp/EtsMobileApp/KisilerCell/TableViewCell.swift
@@ -13,6 +13,17 @@ class TableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        shadowSet()
+        dashedLine()
+        
+        
+        
+        
+    }
+    
+    func shadowSet() {
+        
+        //cell'ler arası shadow ayarlandı
         anaView.layer.cornerRadius = 8
         let myColor = #colorLiteral(red: 0.7450980392, green: 0.6862745098, blue: 0.7960784314, alpha: 1)
         anaView.layer.shadowColor = myColor.cgColor
@@ -20,6 +31,19 @@ class TableViewCell: UITableViewCell {
         anaView.layer.shadowOpacity = 0.4
         anaView.layer.shadowRadius = 10
         self.backgroundColor = UIColor.clear
+    }
+    //cell içerisinde bulunan dashLine tanımlanıp konumlandırıldı
+    func dashedLine() {
+        let width = UIScreen.main.bounds.width
+        let rect = CGRect.init(origin: CGPoint.init(x: 21, y: 46), size: CGSize.init(width: width - 90, height: 0))//Set Height width as you want
+        let layer = CAShapeLayer.init()
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: 0)
+        layer.path = path.cgPath;
+        layer.strokeColor = #colorLiteral(red: 0.4392156863, green: 0.4392156863, blue: 0.4392156863, alpha: 1).cgColor; // Set Dashed line Color
+        layer.lineDashPattern = [3,5]; // Here you set line length
+        layer.backgroundColor = UIColor.clear.cgColor;
+        layer.fillColor = UIColor.clear.cgColor;
+        self.anaView.layer.addSublayer(layer);
     }
 
     
